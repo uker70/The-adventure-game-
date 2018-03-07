23058e1d76c987545d99bee75eed93805a4ea9c2
benjamin ostner <benjamin110101@gmail.com>
2018-03-07 12:31:27 +0100

    Add project files.

---
+++ The adventure(game)/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_adventure_game_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
